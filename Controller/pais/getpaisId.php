c59aef9126ac73d153b4c08cd5e30bffd83a561e
he03villa <he03villa@gmail.com>
2017-10-23 18:30:51 -0500

    agregando el controlador

--- Controller/pais/getpaisId.php
+++ Controller/pais/getpaisId.php
@@ -5,8 +5,12 @@
         if(isset($_GET['values'])){
             $valor = $_GET['values'];
             $respuesta = Pais::getPaisId($valor);
-            if($respuesta) echo json_encode($respuesta);
+            if($respuesta){
+                $contenedor["respuesta"] = "CC";
+                $contenedor["dato"] = $respuesta;
+                echo json_encode($contenedor);
+            }
             else echo json_encode(array('respuesta'=>'No se encuentra el pais'));
-        } 
+        }else echo json_encode(array('respuesta'=>'No se permite datos en blanco'));
     }
 ?>
