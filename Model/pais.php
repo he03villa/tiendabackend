1866c5a10e1368e9fb58e56495d34591e39edfce
he03villa <he03villa@gmail.com>
2017-10-23 18:07:44 -0500

    agregando una nueva funcion en la clse pais

--- Model/pais.php
+++ Model/pais.php
@@ -1,21 +1,42 @@
 <?php
     require 'Database.php';
-    Pais::lista();
+    
     class Pais{
         function __construct() {
             
         }
         
         public static function lista(){
+            $consulta = "SELECT id,nombre FROM pais";
+            $resultado = Database::getInstance()->getDb()->prepare($consulta);
+            $resultado->execute();
+            $tabla=$resultado->fetchAll(PDO::FETCH_ASSOC);
+            return $tabla;   
+        }
+        
+        public static function getPaisNo($values){
             try {
-                $consulta = "SELECT id,nombre FROM pais";
+                $consulta = "SELECT id,nombre FROM pais WHERE nombre=?";
                 $resultado = Database::getInstance()->getDb()->prepare($consulta);
-                $resultado->execute();
+                $resultado->execute(array($values));
                 $tabla=$resultado->fetchAll(PDO::FETCH_ASSOC);
-                echo json_encode($tabla);   
+                return $tabla;   
             } catch (Exception $ex) {
-               echo 'error';
+                return FALSE;
             }
+            
+        }
+        public static function getPaisId($values){
+            try {
+                $consulta = "SELECT id,nombre FROM pais WHERE id=?";
+                $resultado = Database::getInstance()->getDb()->prepare($consulta);
+                $resultado->execute(array($values));
+                $tabla=$resultado->fetchAll(PDO::FETCH_ASSOC);
+                return $tabla;   
+            } catch (Exception $ex) {
+                return FALSE;
+            }
+            
         }
     }
 ?>
