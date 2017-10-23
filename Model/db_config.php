4265b1b9a10b85a8e9bda2fa2e17364629bec8fc
he03villa <he03villa@gmail.com>
2017-10-23 15:34:52 -0500

    subiendo archivo

---
+++ Model/db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "id3366196_root"); // Usuario
+define('DB_PASSWORD', "ivored"); // Password
+define('DB_DATABASE', "id3366196_tienda"); // Nombre de la base de datos
+define('DB_SERVER', "localhost"); // db server
+?>
