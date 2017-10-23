525f630e36f41a983ab33f3ea33a7aaa2daae3c5
he03villa <he03villa@gmail.com>
2017-10-23 16:42:19 -0500

    arreglando la clse pais

--- Model/pais.php
+++ Model/pais.php
@@ -11,7 +11,7 @@
                 $consulta = "SELEC id,nombre FROM pais";
                 $resultado = Database::getInstance()->getDb()->prepare($consulta);
                 $resultado->execute();
-                $tabla=$resultado->fecthAll(PDO::FETCH_ASSOC);
+                $tabla=$resultado->fetchAll(PDO::FETCH_ASSOC);
                 echo json_decode($tabla);   
             } catch (Exception $ex) {
                 echo 'error';
