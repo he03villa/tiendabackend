6fbec8536e7220890c6a1f5840739aadad044708
he03villa <he03villa@gmail.com>
2017-10-27 00:05:37 -0500

    agregando el modelo de departamento con sus funciones

---
+++ Model/departamento.php
@@ -0,0 +1,64 @@
+<?php
+    require 'Database.php';
+    
+    class Departamento{
+        function __construct() {}
+        
+        public static function lista(){
+            $consulta = "SELECT d.id,d.nombre,p.nombre FROM departamento d INNER JOIN pais p on p.id=d.id";
+            $resultado = Database::getInstance()->getDb()->prepare($consulta);
+            $resultado->execute();
+            $tabla=$resultado->fetchAll(PDO::FETCH_ASSOC);
+            return $tabla;   
+        }
+        
+        public static function getDepartamentoN($name){
+            $consulta = "SELECT d.id,d.nombre,p.nombre FROM departamento d INNER JOIN pais p on p.id=d.id WHERE nombre=?";
+            $resultado = Database::getInstance()->getDb()->prepare($consulta);
+            $resultado->execute(array($name));
+            $tabla=$resultado->fetch(PDO::FETCH_ASSOC);
+            return $tabla;   
+        }
+        
+        public static function getDepartamentoId($id){
+            $consulta = "SELECT d.id,d.nombre,p.nombre FROM departamento d INNER JOIN pais p on p.id=d.id WHERE id=?";
+            $resultado = Database::getInstance()->getDb()->prepare($consulta);
+            $resultado->execute(array($id));
+            $tabla=$resultado->fetch(PDO::FETCH_ASSOC);
+            return $tabla;   
+        }
+        
+        public static function Insertar($name,$pais){
+            try {
+                $consulta = "INSERT INTO departamento(nombre,pais) VALUES('".$name."','".$pais."')";
+                $ressultado = Database::getInstance()->getDb()->prepare($consulta);
+                $ressultado->execute();
+                return "El pagis se guardo exitosamente";
+            } catch (Exception $ex) {
+                return "error_1".$ex;
+            }
+        }
+        
+        public static function Actualizar($id,$name,$pais){
+            try {
+                $consulta = "UPDATE departamento SET nombre='".$name."',pais=".$pais." WHERE id=".$id;
+                $ressultado = Database::getInstance()->getDb()->prepare($consulta);
+                $ressultado->execute();
+                return "El pagis se actualizado exitosamente";
+            } catch (Exception $ex) {
+                return "error_1".$ex;
+            }
+        }
+        
+        public static function Eliminar($name){
+            try {
+                $consulta = "DELETE FROM departamento where nombre='".$name."'";
+                $ressultado = Database::getInstance()->getDb()->prepare($consulta);
+                $ressultado->execute();
+                return "El pagis se elimino exitosamente";
+            } catch (Exception $ex) {
+                return "error_1".$ex;
+            }
+        }
+    }
+?>
